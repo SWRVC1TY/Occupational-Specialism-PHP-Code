e5fee477597abe1dea4a08fe8b90b122f4ca983c
SWRVC1TY <harris.butt31@gmail.com>
2025-09-05 10:41:33 +0100

    This is some PHP code that says my name and a message

---
+++ HTML_Basics/V1/index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+    <title>harris</title>
+
+
+    </head>
+    <body>
+    <h1>harris</h1>
+    <?php // opens php code section
+    echo "this is my page"; // displays text
+
+    ?>
+    </body>
+</html>
+
