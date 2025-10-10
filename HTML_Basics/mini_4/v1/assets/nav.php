d681d28d9f9e38800e7394fc2dfe8f284f1d2864
SWRVC1TY <harris.butt31@gmail.com>
2025-10-10 10:24:05 +0100

    started mini 4

---
+++ HTML_Basics/mini_4/v1/assets/nav.php
@@ -0,0 +1,15 @@
+<?php
+// Navigation Section
+echo "<div id = 'navi'>"; // Opens navigation block
+echo "<nav>";
+echo "<ul>";
+
+// Navigation Buttons (each in a table cell)
+echo "<li><a href='index.php'>Home</a></li>";   // Link to View Bookings page
+echo "<li><a href='page1.php'>Page1</a></li>";  // Link to page 1
+echo "<li><a href='page2.php'>Page2</a></li>";  // Link to page 2
+echo "<li><a href='page3.php'>Page3</a></li>";  // Link to page 3
+echo "</ul>";
+
+echo"<nav>";
+echo "</div>";
---
+++ HTML_Basics/mini_4/v1/assets/top_bar.php
@@ -0,0 +1,5 @@
+<?php
+echo "<div class='topbar'>";
+// Logo Image
+echo "<img src='images/imagename.png' alt='banner' />";  // Displays the website banner
+echo "</div>";
